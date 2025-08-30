58b78ea12397eff3c883f83ba65538f748d784df
Elston Misquitta <elston007@gmail.com>
2025-08-30 17:29:32 +0530

    Code added for Sample Project
    Code added for Sample Project

---
+++ ContosoPizzaApp/ContosoPizza/Data/ContosoPizzaContext.cs
@@ -0,0 +1,22 @@
+﻿using ContosoPizza.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContosoPizza.Data;
+
+public class ContosoPizzaContext : DbContext
+{
+    public DbSet<Customer> Customers { get; set; } = null!;
+    public DbSet<Order> Orders { get; set; } = null!;
+    public DbSet<Product> Products { get; set; } = null!;
+    public DbSet<OrderDetail> OrderDetails { get; set; } = null!;
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(@"Server=LAPTOP-O1B8UMOG;Database=ContosoPizza;User Id=sa;Password=phoenixuser;Encrypt=True;TrustServerCertificate=True;");
+    }
+}
